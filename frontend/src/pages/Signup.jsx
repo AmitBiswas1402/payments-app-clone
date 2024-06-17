901869db6af6e640107bfb051f41ddf5ee1d06a8
Amit Biswas <amit.142biswas@gmail.com>
2024-06-17 22:45:29 +0530

    whatever

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -38,7 +38,7 @@ export const Signup = () => {
             onChange={(e) => {
               setUsername(e.target.value);
             }}
-            placeholder="harkirat@gmail.com"
+            placeholder="email@gmail.com"
             label={"Email"}
           />
           <InputBox
