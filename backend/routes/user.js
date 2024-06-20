5697c7fb23b6a2dd29302a24545cd82c8ede8991
Amit Biswas <amit.142biswas@gmail.com>
2024-06-20 16:38:48 +0530

    sign in schema made

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -39,7 +39,40 @@ router.post("/signup", async (req, res) => {
         message: "User Created Successfully",
         token: token,
     })
+})
 
+const signinSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
+
+router.post("/signin", async (req, res) => {
+    const {success} = signinSchema.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email or Password Incorrect"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id 
+        }, JWT_SECRET)
+
+        res.json({
+            token: token
+        })
+        return
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
 })
 
 modile.exports = router
